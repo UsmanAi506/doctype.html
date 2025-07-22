880632fa6bbdf4f45e4b36df1ed2d65473f147e1
UsmanAi506 <usmanmalik86.mm@outlook.com>
2025-07-22 10:25:33 +0500

    Create adsbygoogle.js

---
+++ adsbygoogle.js
@@ -0,0 +1,16 @@
+<!-- In the head section -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5884815764517957" crossorigin="anonymous"></script>
+
+<!-- In the ad spaces -->
+<div class="ad-space">
+    <p>Advertisement</p>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-5884815764517957"
+         data-ad-slot="1234567890"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
